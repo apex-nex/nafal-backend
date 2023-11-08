6d7813bc6eda3d374ed662389f6adfb249ab7e7d
Mohammad Adnan <mdadnanengg@gmail.com>
2023-11-09 01:30:57 +0530

    login function added

--- server.js
+++ server.js
@@ -2,16 +2,25 @@ import express from "express"
 import dotenv from 'dotenv'
 import routerLogin from "./routes/loginRouter.js"
 import connectDB from "./db/connect.js";
+import routerRegister from "./routes/registerRouter.js";
+import cors from 'cors'
 
 dotenv.config();
 const app = express()
 const port = process.env.PORT || 9000;
 const db_url = process.env.DB_URL
 
+// setup cors
+app.use(cors())
+
+// middleware for register admin
+app.use('/admin', routerRegister)
+
+// middleware for login admin
+app.use('/admin', routerLogin)
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
-app.use('/admin', routerLogin)
 
 connectDB(db_url)
 
