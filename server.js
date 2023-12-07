6e0517d80326396a843e662207ae19a4aa52237f
apex-nex <148151115+apex-nex@users.noreply.github.com>
2023-12-07 21:10:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,13 +3,11 @@ import dotenv from 'dotenv'
 import connectDb from "./utils/db/connect.js";
 import routerAdmin from "./routes/adminRouter.js";
 import cors from 'cors'
-import http from 'http'
 import routerForm from "./routes/formRouter.js";
 import errorMiddleware from "./middlewares/errorMiddleware.js";
 
 dotenv.config();
 const app = express()
-const server = http.createServer(app);
 const port = process.env.PORT || 9000;
 const db_url = process.env.DB_URL
 
@@ -28,21 +26,6 @@ app.use(express.urlencoded({ extended: false }))
 
 // error handling
 app.use(errorMiddleware)
-
-// to active server
-app.get('/ping', (req, res) => {
-    res.status(200).send('All Is Well.');
-    console.log("All Is Well.")
-});
-
-// Periodically ping the server every 10 minutes
-const pingInterval = 10 * 60 * 1000; // 10 minutes in milliseconds
-
-setInterval(() => {
-    http.get(`http://nafal.onrender.com/:${port}/ping`, (res) => {
-        console.log('All Is Well.', res.statusCode);
-    });
-}, pingInterval);
 
 // connection and start port
 connectDb(db_url).then(() => {
